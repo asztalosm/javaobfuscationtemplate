8816eb75985c4475adc252a78d7a98be7b378d5f
asztalosm <23d_asztalosm@nyirszikszi.hu>
2025-09-18 16:55:49 +0200

    textentry

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<title>Java obfuscator</title>
+<style>
+    :global(html){
+        background-color: rgb(59, 59, 59);
+        color: rgb(240, 240, 240)
+    }
+
+    #codeholder{
+        text-align: center;
+        min-height: 80%;
+        height:80vh;
+        background-color: rgb(42,42,42);
+        border-color: rgb(30,30,30);
+        margin: 1% 0;
+    }
+    .buttonshit {
+        text-align: center;
+        margin: 0 auto;
+        padding: 1%;
+        background-color: rgb(42,42,42);
+        border-color: rgb(30,30,30);
+    }
+    h1 {
+        font-size: xx-large;
+    }
+</style>
+
+<script>
+    import "../app.css"
+</script>
+<div class="appbg">
+<h1 class="text-center">Java obfuscator</h1>
+<p class="text-center">just makes your java code look like ass, why?</p>
+<center><textarea type="text" name="code" placeholder="Insert Java code to Dogshittify" id="codeholder" class="w-10/12 object-center content-center border-gray-700 border-solid border-2 rounded-xl bg-gray-900"></center></textarea>
+<center><button type="button" class="text-center buttonshit border-gray-900 border-2 rounded-xl">Doghittify 🚮</button></center>
+</div>
